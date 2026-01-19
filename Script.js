d4de1cef31f2dc600714ec7272a4ede9d3e4606d
Mohan071019 <mohanmb071999@gmail.com>
2026-01-19 21:22:51 +0530

    xxx

---
+++ Script.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<body>
+<p id="my title">Hello world</p> 
+<h1>Hello</h1>
+<script>
+const title=documet.queryselector("#mytitle");
+console,console.log(title);
+</script>  
+</body>
+
+
